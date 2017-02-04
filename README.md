51260196b813628dbdde6f15acac94a9370a2e12
kimkkikki <kimkkikki1@gmail.com>
2017-02-04 17:17:31 +0900

    add memcache

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # daesun
 
 - brew install python3
+- brew install libmemcached
+- brew install memcached
+- brew services start memcached
+
 - pip install virtualenv
-- mkvirtualenv -p python3 daesun
+- virtualenv -p python3 daesun
 
 - pip install django 
 - pip install mysqlclient
+- pip install pylibmc
--- apis/views.py
+++ apis/views.py
@@ -2,10 +2,28 @@ from apis.models import Scraps
 from django.http import HttpResponse
 from django.db.models import Count
 from django.db.models import Q, Case, When
-import json, os
+import json, os, pylibmc
 
-if os.getenv('GAE_INSTANCE'):
-    from google.appengine.api import memcache
+
+def get_memcache_client():
+    # Environment variables are defined in app.yaml.
+    # Note: USE_GAE_MEMCACHE is in whitelist-only alpha. See README.md
+    if os.environ.get('USE_GAE_MEMCACHE'):
+        memcache_server = ':'.join([
+            os.environ.get('GAE_MEMCACHE_HOST', 'localhost'),
+            os.environ.get('GAE_MEMCACHE_PORT', '11211')])
+    else:
+        memcache_server = os.environ.get('memcache_server', 'localhost:11211')
+
+    memcache_username = os.environ.get('MEMCACHE_USERNAME')
+    memcache_password = os.environ.get('MEMCACHE_PASSWORD')
+
+    memcache_client = pylibmc.Client(
+        [memcache_server], binary=True,
+        username=memcache_username, password=memcache_password)
+
+    return memcache_client
+
 
 def index(request):
     scraps = Scraps.objects.all().values('title', 'cp')
@@ -13,11 +31,12 @@ def index(request):
 
 
 def group(request):
-    if os.getenv('GAE_INSTANCE'):
-        result = memcache.get('group_db_result')
+    client = get_memcache_client()
+    result = client.get('group_db_result')
 
-        if result:
-            return HttpResponse(result, content_type='application/json; charset=utf-8')
+    if result:
+        print('memcache hit')
+        return HttpResponse(result, content_type='application/json; charset=utf-8')
 
     group_list = Scraps.objects.filter(
         Q(title__contains='문재인') | Q(title__contains='안철수') | Q(title__contains='이재명') |
@@ -34,7 +53,7 @@ def group(request):
 
     result = json.dumps(list(group_list))
 
-    if os.getenv('GAE_INSTANCE'):
-        memcache.add(key='group_db_result', value=result, time=3600)
+    client.add(key='group_db_result', val=result, time=3600)
 
+    print('memcache not hit')
     return HttpResponse(json.dumps(list(group_list)), content_type='application/json; charset=utf-8')
