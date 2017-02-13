45ab16353355bf74048c598efc3c9c62e03db9fe
kimkkikki <kimkkikki1@gmail.com>
2017-02-13 15:50:04 +0900

    add url /graphs

--- web/views.py
+++ web/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, template_name='index.html')
+
+
+def graphs(request):
+    return render(request, template_name='graphs.html')
