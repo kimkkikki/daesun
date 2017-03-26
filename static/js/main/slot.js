199e60a655c50749f7d2a2b236a288b71415da18
송민정 <minchocosong@gmail.com>
2017-03-26 23:42:20 +0900

    공유 후 명예의전당 부분 추가 / form 넘기는 부분 ajax로 변경해야함

--- static/js/main/slot.js
+++ static/js/main/slot.js
@@ -8,7 +8,7 @@ $('#slot-guide-button').click(function () {
 
 function share_facebook(){
     waitMe($('#slot-modal'));
-    domtoimage.toJpeg(document.getElementById('slot-result'), { quality: 0.95 })
+    domtoimage.toJpeg(document.getElementById('slot-result-detail'), { quality: 0.95 })
         .then(function (dataUrl) {
         $.ajax({
             url:'/apis/upload',
@@ -22,7 +22,9 @@ function share_facebook(){
             success:function(data){
                 console.log(data);
                 $('#slot-modal').waitMe('hide');
-                snsShare('facebook', data, null);
+                //snsShare('facebook', data, null);
+                $('#your_name').removeAttr('disabled');
+                $('#add_honor').removeAttr('disabled');
             },
             error: function(data, status, err) {
                 $('#slot-modal').waitMe('hide');
@@ -38,7 +40,7 @@ function share_facebook(){
 
 function share_kakaotalk(){
     waitMe($('#slot-modal'));
-    domtoimage.toJpeg(document.getElementById('slot-result'), { quality: 0.95 })
+    domtoimage.toJpeg(document.getElementById('slot-result-detail'), { quality: 0.95 })
         .then(function (dataUrl) {
             $.ajax({
                 url:'/apis/upload',
@@ -53,6 +55,8 @@ function share_kakaotalk(){
                     console.log(data);
                     $('#slot-modal').waitMe('hide');
                     snsShare('kakaotalk', data, null);
+                    $('#your_name').removeAttr('disabled');
+                    $('#add_honor').removeAttr('disabled');
                 },
                 error: function(data, status, err) {
                     $('#slot-modal').waitMe('hide');
@@ -99,7 +103,7 @@ function onComplete(active){
             result_3 = active;
 
             slotStart.prop('disabled', false);
-            //if (result_1 == result_2 && result_2 == result_3) {
+            if (result_1 == result_2 && result_2 == result_3) {
                 waitMe($('#slot'));
                 slot_result_candidate = slot_candidates[result_1];
                 slot_result_count = slot_count;
@@ -127,11 +131,11 @@ function onComplete(active){
                             $('#slot').waitMe('hide');
                         }
                     });
-//            } else {
-//                var messages = ['까비 다시 도전해보세요', '후보 고르기 슆지 않죠. 다시고고!', '힘내세요. 다시 도전!', '슬슬 포기단계? 힘내서 고고!', '확률은 확률일 뿐. 다시 도전!', 'ㅠㅠ', '까비요.'];
-//                slotModalCount.attr('data-original-title', messages[Math.floor((Math.random() * 8))])
-//                  .tooltip('show');
-//            }
+            } else {
+                var messages = ['까비 다시 도전해보세요', '후보 고르기 슆지 않죠. 다시고고!', '힘내세요. 다시 도전!', '슬슬 포기단계? 힘내서 고고!', '확률은 확률일 뿐. 다시 도전!', 'ㅠㅠ', '까비요.'];
+                slotModalCount.attr('data-original-title', messages[Math.floor((Math.random() * 8))])
+                  .tooltip('show');
+            }
             break;
     }
 }
@@ -146,19 +150,29 @@ slotStart.click(function(){
     $('#slot-machine-count').tooltip('hide');
 });
 
-$('#slot-honor-button').click(function () {
-    waitMe($('#slot'));
-    slotModalCount.tooltip('hide');
-    $.ajax({
-            url: '/slot/honor',
+function check(){
+    if( $('#your_name')[0].disabled == true){
+        $('#add-honor-form').attr('data-original-title', '소셜 공유 후에 명예의 전당에 등록하실 수 있습니다').tooltip('show');
+    }else{
+        $('#add-honor-form').removeAttr('data-original-title');
+    }
+}
+
+
+$(document).ready(function(){
+
+    $('#add_honor').click(function () {
+    alert('click');
+        $.ajax({
+            url: '/apis/slot/add',
             headers: {
                 'Content-Type':'application/json'
             },
             async: true,
-            type: 'GET',
+            type: 'POST',
+            data: $('#form').serialize(),
             success: function(data) {
-                $('#slot-honor-result').html(data);
-                $('#slot-honor-modal').modal('show');
+                console.log('done');
                 $('#slot').waitMe('hide');
             },
             error: function(data, status, err) {
@@ -166,25 +180,6 @@ $('#slot-honor-button').click(function () {
                 $('#slot').waitMe('hide');
             }
         });
-
-});
-
-$('#add_honor').click(function(){
-    alert('add');
-    //action="/apis/slot/add" method="post"
-    $.ajax({
-        url: '/apis/slot/add',
-        async: true,
-        type: 'POST',
-        data: $('#form').serialize(),
-        success: function(data) {
-            $('#slot-honor-result').html(data);
-            $('#slot-honor-modal').modal('show');
-            $('#slot').waitMe('hide');
-        },
-        error: function(data, status, err) {
-            console.log(err);
-            $('#slot').waitMe('hide');
-        }
     });
+
 });
